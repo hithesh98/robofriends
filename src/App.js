7a1b27a98d93d080a0e84b91bdf34a1ebb47613c
hithesh98 <kirrkirry@gmail.com>
2021-03-02 20:01:57 +0000

    add searchbox

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import { render } from '@testing-library/react';
+import React, {Component} from 'react'
 import CardList from './CardList';
 import { robots } from './robots';
 import Searchbox from './Searchbox'
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>Robofriends</h1>
-            <Searchbox />
-            <CardList  robots={robots}/>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield:''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())       })
+
+        return (
+            <div className='tc'>
+                <h1>Robofriends</h1>
+                <Searchbox searchChange={this.onSearchChange}/>
+                <CardList  robots={filteredRobots}/>
+            </div>
+        );
+    }
+   
 }
 export default App
